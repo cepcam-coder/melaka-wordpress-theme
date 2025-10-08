dbfe920b6f1fc4490bdfef2bfd433423d7e7e7a1
arnold.zephir@gmail.com <arnold.zephir@gmail.com>
2025-10-08 18:20:04 +0200

    feat: worpdress theme boilerplate

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function melaka_enqueue_styles() {
+    wp_enqueue_style(
+        'melaka-style',
+        get_theme_file_uri('/assets/css/melaka.css'),
+        array(),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'melaka_enqueue_styles');
