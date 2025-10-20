3ee5e5695e6a280673cf518639a62ecd92b75b33
arnold.zephir@gmail.com <arnold.zephir@gmail.com>
2025-10-20 18:31:29 +0200

    chore: sync test

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer style="text-align:center;padding:10px 0;max-width:950px;margin:0 auto;">
-    <p>© 2025 - Mentions légales | Contact</p>
+    <p>© 2025 - Mentions légales | Par Cepcam</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
