b4f499c204154716148157d30d4311cb7444e15e
arnold.zephir@gmail.com <arnold.zephir@gmail.com>
2025-10-22 14:58:43 +0200

    feat: search form and search result

---
+++ search.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+<div id = "wrapper" class="main-column">
+    <main id="main">
+        <div id="content" class="item-list">
+            <?php
+            if (have_posts()):
+                while (have_posts()):the_post();
+                /** Small header before post content */
+            ?>
+                    <div class="item">
+                    <h2 class="post-title">
+                        <img class="avatar" src="<?php echo get_template_directory_uri(); ?>/assets/css/img/melaka.png" alt="avatar">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+        
+                    <p class="post-info">le <?php echo get_the_date('l j F Y'); ?> à <?php echo get_the_time(); ?></p>
+                    </div>
+            <?php
+                    
+                endwhile;
+            endif;
+            ?>
+            <p class="pagination">
+            <?php
+                global $wp_query;
+
+                $total_pages = $wp_query->max_num_pages;
+                $current_page = max(1, get_query_var('paged'));    
+                if (get_next_posts_link()) {
+                    next_posts_link('« entrées précédentes');
+                }
+                echo ' - page ' . $current_page . ' de ' . $total_pages;
+                if (get_previous_posts_link()) {
+                    if (get_next_posts_link()) echo ' | ';
+                    previous_posts_link('entrées suivantes »');
+                }
+            
+
+            ?>
+            </p>
+        </div>
+    </main>
+
+    <div id="sidebar"><?php get_sidebar(); ?></div>
+</div>
+
+<?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,13 @@
 <aside>
 
     <div id="blognav">
+
+        <section class="sidebar-section">
+        <?php get_search_form(); ?>
+        </section>
+        <hr class="doodle-separator doodle-1">
+
+        
         <h4>Archives</h4>
         <ul>
             <?php wp_get_archives(array('type' => 'monthly', 'limit' => 18)); ?>
