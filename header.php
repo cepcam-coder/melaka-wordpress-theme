cce36ea863717a20e851335f4c31328a6c8bb585
arnold.zephir@gmail.com <arnold.zephir@gmail.com>
2025-11-03 09:46:40 +0100

    feat: new banner

--- header.php
+++ header.php
@@ -16,19 +16,23 @@
             <a href="index.php">
                 <!-- PARALLAX -->
                 <div id="parallax" class="parallax-viewport">
-                    <div class="parallax-layer" style="width:1000px; height:200px;">
-                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/melaka_banner_1.png') ?>" alt=""
-                            style="width:1000px; height:200px;" />
+                    <div class="parallax-layer" style="width:1200px; height:200px;">
+                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/fond_logo.png') ?>" alt=""
+                            style="width:1200px; height:200px;" />
                     </div>
                     <div class="parallax-layer" style="width:1100px; height:200px;">
-                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/melaka_banner_2.png') ?>" alt=""
+                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/troisiemeplan.png') ?>" alt=""
                             style="width:1100px; height:200px;" />
                     </div>
                     <div class="parallax-layer" style="width:1200px; height:200px;">
-                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/melaka_banner_3.png') ?>" alt=""
+                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/secondplan.png') ?>" alt=""
                             style="width:1200px; height:200px;" />
                     </div>
-                    <div class="parallax-layer" style="padding-left:890px;height:200px">
+                    <div class="parallax-layer" style="width:1200px; height:200px;">
+                        <img src="<?php echo get_theme_file_uri('/assets/css/img/banner/premierplan.png') ?>" alt=""
+                            style="width:1200px; height:200px;" />
+                    </div>
+                    <div class="parallax-layer" style="padding-left:900px;height:200px">
                         <img src="<?php echo get_theme_file_uri('/assets/css/img/random/rotate.php') ?>" alt=""                        
                     </div>
                 </div>
